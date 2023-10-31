7d967275afe1c1f5de8bee26b0333a654755280e
AlexYBS <114355594+AlexYBS@users.noreply.github.com>
2023-10-31 23:55:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+//var name = prompt("Salut,spune-mi cum te numesti");
+var sliceNameA = name.slice(0, 1);
+var sliceNameB = name.slice(1,name.length);
+//alert("Salut " + sliceNameA.toUpperCase() + sliceNameB.toLowerCase()+ " , bine ai venit pe site-ul meu!☺️❤️✂");
+//alert( sliceNameA.toUpperCase() + sliceNameB.toLowerCase()+ " as vrea sa stii ca acest site este momentan un prototip,si ca inca se lucreaza din greu pentru a incerca sa il imbunatatim.")
+//alert( sliceNameA.toUpperCase() + sliceNameB.toLowerCase()+" poti arunca o scurta privire si poate incerci si o programare !!!☺️❤️✂")
